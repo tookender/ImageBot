292bfcc6195968e493ebdaea3ad4fa3bcdac1d6a
Ender <68495763+Ender2K89@users.noreply.github.com>
2023-01-14 18:52:05 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 BOT_TOKEN = ""
 EXTENSIONS = ["extensions.image"]
+PROFILE_PNG_PATH = "C:\\Users\\ender\\Desktop\\Development\\Customers\\ImageBot\\profile.png" # The path to the profile.png file
+COOLDOWN_PER = 1 # How many times the command can be used before cooldown starts
+COOLDOWN_LENGTH = 60 # How long the cooldown lasts in seconds
